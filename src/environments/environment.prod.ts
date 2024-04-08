ca44b67af22c8ce306d287e3980330be8164ce9f
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-08 17:15:23 +0530

    updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDBHvfa2UuJRPQOW7BytE57nHHH5mf7zNM',
+    authDomain: 'ecommerce-shop-a878d.firebaseapp.com',
+    projectId: 'ecommerce-shop-a878d',
+    storageBucket: 'ecommerce-shop-a878d.appspot.com',
+    messagingSenderId: '44495925992',
+    appId: '1:44495925992:web:302c8bb9a614dc76e54f5d',
+  },
+  production: true,
+};
